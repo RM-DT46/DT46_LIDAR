a015871423252484b7a5e69b89521418b5f9c1aa
GlennAde <1519623572@qq.com>
2025-09-16 20:28:31 +0800

    refactor(begin.sh): 更新环境路径和地图文件位置
    - 更新 ROS 2 环境路径：从 /home/morefine/rm2025_hzu_sentry_ws/install/setup.bash 更改为 /home/dt46/rm2025_hzu_sentry_ws/install/setup.bash
    - 更新地图文件路径：从 /home/morefine/rm2025_hzu_sentry_ws/slam_map.yaml 更改为 /home/dt46/rm2025_hzu_sentry_ws/slam_map.yaml

--- begin.sh
+++ begin.sh
@@ -3,12 +3,12 @@
 
 echo "Sourcing ROS 2 environment..."
 source /opt/ros/humble/setup.bash
-source /home/morefine/rm2025_hzu_sentry_ws/install/setup.bash
+source /home/dt46/rm2025_hzu_sentry_ws/install/setup.bash
 
 echo "Launching processes in separate terminals..."
 
 gnome-terminal --tab --title="test_all.launch.py" -- bash -c "ros2 launch bot_navigation2 test_all.launch.py; exec bash"
-gnome-terminal --tab --title="navigation2_copy.launch.py" -- bash -c "ros2 launch bot_navigation2 navigation2_copy.launch.py use_sim_time:=False slam:=False map:=/home/morefine/rm2025_hzu_sentry_ws/slam_map.yaml; exec bash"
+gnome-terminal --tab --title="navigation2_copy.launch.py" -- bash -c "ros2 launch bot_navigation2 navigation2_copy.launch.py use_sim_time:=False slam:=False map:=/home/dt46/rm2025_hzu_sentry_ws/slam_map.yaml; exec bash"
 gnome-terminal --tab --title="nav2_to_goal" -- bash -c "ros2 run map_tools nav2_to_goal; exec bash"
 gnome-terminal --tab --title="cmd_vel2serial.py" -- bash -c "ros2 run map_tools cmd_vel2serial.py; exec bash"
 gnome-terminal --tab --title="odo" -- bash -c "ros2 run map_tools odo; exec bash"
